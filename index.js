9e766c8c86ec4dfbf3e0f581d8c142f2c1de9777
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-24 19:36:59 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ");
+const [blackAndWhite, ,black, pink] = farmAnimals.split(" ");
+const [red, orange, yellow, green, blue, indigo, violet] = colors 
+const [r, o, y, g, b, , v] = colors;
+const [,,,,,indg] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -24,10 +30,13 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-
+const { muppetName, color, song, job, partner } = muppet;
+const { album: { theMuppetMovie: { song2, song4 } }} = nestedMuppet
+const {nestedJob, nestedPartner} = nestedMuppet
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
@@ -44,5 +53,5 @@ const nestedMuppet = {
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
-
+ 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
